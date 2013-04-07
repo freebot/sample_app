f994bf8d259f72d310aa47ef4c0c58f983233afd
Freebot <freebot@freebot-AOA150.(none)>
2013-04-06 18:16:19 -0600

    Primer init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3cfa9b2d8741cd1a1e83809d7d29e6bb36ba104b53a72a427f5ed711bcd97fd45b5063fe997a16413a6bd000148f9f9ff9a86ad09f8f36113b133b600d779925'
